6a5fcd51e5c05cd470a533d24e6b6816bd3e758e
Yudchits <pashka.yudchits@gmail.com>
2024-08-11 18:16:16 +0300

    Added models

---
+++ FitLibrary/Models/Exercise.cs
@@ -0,0 +1,13 @@
+﻿namespace FitLibrary.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public int Week { get; set; }
+        public string ExerciseName { get; set; }
+        public int Sets { get; set; }
+        public int Repetitions { get; set; }
+        public double Weight { get; set; }
+        public string RestPeriod { get; set; }
+    }
+}
