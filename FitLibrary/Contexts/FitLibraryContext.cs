cc43af6b290acad955637047f886b3a68ed6e428
Yudchits <pashka.yudchits@gmail.com>
2024-08-16 17:11:05 +0300

    Added training plan repository

--- FitLibrary/Contexts/FitLibraryContext.cs
+++ FitLibrary/Contexts/FitLibraryContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FitLibrary.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitLibrary.Contexts
 {
     public class FitLibraryContext : DbContext
     {
+        public DbSet<TrainingPlan> TrainingPlans { get; set; }
+
+        public FitLibraryContext(DbContextOptions<FitLibraryContext> options) : base(options)
+        {
+        }
     }
 }
---
+++ FitLibrary/Repositories/ITrainingPlanRepository.cs
@@ -0,0 +1,16 @@
+﻿using FitLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FitLibrary.Repositories
+{
+    public interface ITrainingPlanRepository
+    {
+        Task<ICollection<TrainingPlan>> GetAllTrainingPlansAsync();
+        Task<TrainingPlan> GetTrainingPlanByIdAsync(int id);
+        Task<int> CreateTrainingPlanAsync(TrainingPlan plan);
+        Task<int> UpdateTrainingPlanAsync(TrainingPlan plan);
+        Task<int> DeleteTrainingPlanAsync(TrainingPlan plan);
+        Task<bool> SaveChangesAsync();
+    }
+}
