5293ef86458fb51b01edba003186bca3ce0fa272
Yudchits <pashka.yudchits@gmail.com>
2024-08-16 18:10:16 +0300

    Added DTOs

---
+++ FitLibrary/DTOs/TrainingPlanDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FitLibrary.DTOs
+{
+    public class TrainingPlanDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sport { get; set; }
+        public int CreatorId { get; set; }
+    }
+}
---
+++ FitLibrary/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FitLibrary.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
