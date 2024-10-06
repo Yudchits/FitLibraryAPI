5616991d99c070c595f451f2eefe0b0bc9cc8f6a
Yudchits <pashka.yudchits@gmail.com>
2024-10-06 18:06:02 +0300

    Updated model 'TrainingPlan' of BLL

--- FitLibrary.Logic.Common/Models/TrainingPlanFullBLL.cs
+++ FitLibrary.Logic.Common/Models/TrainingPlanFullBLL.cs
@@ -6,9 +6,11 @@ namespace FitLibrary.Logic.Common.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Sport { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
         public int CreatorId { get; set; }
-        public UserBLL Creator { get; set; }
         public ICollection<ExerciseBLL> Exercises { get; set; }
     }
 }
--- FitLibrary.Logic.Common/Models/TrainingPlanShortBLL.cs
+++ FitLibrary.Logic.Common/Models/TrainingPlanShortBLL.cs
@@ -4,7 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Sport { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
         public int CreatorId { get; set; }
     }
 }
